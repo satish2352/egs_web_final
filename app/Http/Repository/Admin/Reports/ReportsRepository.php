e8e11907d99cebad9493c0ae6a7e75a72f12052a
Savita444 <savita.hajare@sumagoinfotech.com>
2024-03-14 15:53:44 +0530

    changes in report

--- app/Http/Repository/Admin/Reports/ReportsRepository.php
+++ app/Http/Repository/Admin/Reports/ReportsRepository.php
@@ -6,21 +6,43 @@ use DB;
 use Illuminate\Support\Carbon;
 // use Session;
 use App\Models\ {
-	Roles,
-    Permissions,
-    RolesPermissions
+    Labour
 
 };
 
 class ReportsRepository  {
-	public function getAllRole()
-    {
+
+    public function getAllLabourLocation(){
         try {
-            return Roles::all();
+            
+            // $data_output= Labour::get();
+            $data_output = Labour::leftJoin('gender as gender_labour', 'labour.gender_id', '=', 'gender_labour.id')
+        // ->leftJoin('tbl_area as district_labour', 'labour.district_id', '=', 'district_labour.location_id')
+        //   ->leftJoin('tbl_area as taluka_labour', 'labour.taluka_id', '=', 'taluka_labour.location_id')
+        //   ->leftJoin('tbl_area as village_labour', 'labour.village_id', '=', 'village_labour.location_id')
+        //   ->where('gender.is_active', true)
+          ->select(
+              'labour.id',
+              'labour.full_name',
+              'labour.date_of_birth', 
+              'gender_labour.gender_name as name',
+            //   'district_labour.name as district_id',
+            //   'taluka_labour.name as taluka_id',
+            //   'village_labour.name as village_id',
+              'labour.mobile_number',
+              'labour.landline_number',
+              'labour.mgnrega_card_id',
+              'labour.latitude',
+              'labour.longitude',
+            //   'labour.location_id',
+            //   'labour.profile_image',
+            //   'labour.aadhar_image',
+            //   'labour.pancard_image',
+          )->get();
+            return $data_output;
         } catch (\Exception $e) {
+      
             return $e;
         }
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -162,8 +162,10 @@ Route::post('/update-one-documenttype', ['as' => 'update-one-documenttype', 'use
 
 
 // Reports======================
-Route::get('/list-location-report', ['as' => 'list-location-report', 'uses' => 'App\Http\Controllers\Admin\Reports\ReportsController@getAllLocation']);
-
+Route::get('/list-location-report', ['as' => 'list-location-report', 'uses' => 'App\Http\Controllers\Admin\Reports\ReportsController@getAllLabourLocation']);
+Route::get('/list-labour-duration-report', ['as' => 'list-labour-duration-report', 'uses' => 'App\Http\Controllers\Admin\Reports\ReportsController@getAllLabourDuration']);
+Route::get('/list-project-report', ['as' => 'list-project-report', 'uses' => 'App\Http\Controllers\Admin\Reports\ReportsController@getAllProjects']);
+Route::get('/list-project-and-location-report', ['as' => 'list-project-and-location-report', 'uses' => 'App\Http\Controllers\Admin\Reports\ReportsController@getAllProjectLocation']);
 // Route::get('/db-backup', ['as' => 'db-backup', 'uses' => 'App\Http\Controllers\DBBackup\DBBackupController@downloadBackup']);
 
 Route::get('/log-out', ['as' => 'log-out', 'uses' => 'App\Http\Controllers\Admin\LoginRegister\LoginController@logout']);
