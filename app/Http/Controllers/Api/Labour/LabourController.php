65e805d8ac11c5acae4d657658396c0a09cac39f
Savita444 <savita.hajare@sumagoinfotech.com>
2024-03-18 17:06:08 +0530

    changes in api in ad-labor

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -22,7 +22,7 @@ class LabourController extends Controller
     {
         $all_data_validation = [
             'full_name' => 'required|alpha',
-            'gender_id' => 'required|numeric',
+            'gender_id' => 'required',
             // 'date_of_birth' => [
             //     'required',
             //     'date_format:d/m/Y',
@@ -33,25 +33,25 @@ class LabourController extends Controller
             //         }
             //     },
             // ],
-            'district_id' => 'required|numeric',
-            'taluka_id' => 'required|numeric',
-            'village_id' => 'required|numeric',
-            'skill_id' => 'required|numeric',
-            'mobile_number' => ['required', 'numeric', 'digits:10', 'unique:labour'],
+            'district_id' => 'required',
+            'taluka_id' => 'required',
+            'village_id' => 'required',
+            'skill_id' => 'required',
+            'mobile_number' => ['required', 'digits:10', 'unique:labour'],
             
-            'mgnrega_card_id' => ['required', 'numeric', 'unique:labour'],
-            'latitude' => ['required', 'numeric', 'between:-90,90'], // Latitude range
-            'longitude' => ['required', 'numeric', 'between:-180,180'], // Longitude range
+            'mgnrega_card_id' => ['required', 'unique:labour'],
+            'latitude' => ['required', 'between:-90,90'], // Latitude range
+            'longitude' => ['required', 'between:-180,180'], // Longitude range
             // 'aadhar_image' => 'required|image|mimes:jpeg,png,jpg,gif|min:10|max:2048', 
             // 'mgnrega_image' => 'required|image|mimes:jpeg,png,jpg,gif|min:10|max:2048', 
             // 'profile_image' => 'required|image|mimes:jpeg,png,jpg,gif|min:10|max:2048',
             // 'voter_image' => 'required|image|mimes:jpeg,png,jpg,gif|min:10|max:2048',
 
             'family' => 'required|array',
-            'family.*.full_name' => 'required|string',
-            'family.*.gender_id' => 'required|integer',
-            'family.*.relationship_id' => 'required|integer',
-            'family.*.married_status_id' => 'required|integer',
+            'family.*.full_name' => 'required',
+            'family.*.gender_id' => 'required',
+            'family.*.relationship_id' => 'required',
+            'family.*.married_status_id' => 'required',
             'family.*.date_of_birth' => [
                 'required',
                 'date_format:d/m/Y',
