f3c52144ece789309fa3259dd39984441307a01c
Savita444 <savita.hajare@sumagoinfotech.com>
2024-03-19 18:23:47 +0530

    change in api for data filter user updated

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -299,6 +299,9 @@ class LabourController extends Controller
 
             $user_id = Auth::user();
 
+            info('$user_id');
+            info($user_id);
+
             $data_output = Labour::leftJoin('gender as gender_labour', 'labour.gender_id', '=', 'gender_labour.id')
                 ->leftJoin('tbl_area as district_labour', 'labour.district_id', '=', 'district_labour.location_id')
                 ->leftJoin('tbl_area as taluka_labour', 'labour.taluka_id', '=', 'taluka_labour.location_id')
