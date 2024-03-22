bd5a9fb9e5ca87f26b7fe13447ee40fb96b54b82
Savita444 <savita.hajare@sumagoinfotech.com>
2024-03-22 10:00:01 +0530

    changes in api

--- database/migrations/2024_03_10_085414_create_labour.php
+++ database/migrations/2024_03_10_085414_create_labour.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('labour', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('reason_id')->nullable();
             $table->string('full_name');
             $table->string('gender_id');
             $table->string('date_of_birth');
@@ -28,7 +29,6 @@ return new class extends Migration
             $table->string('longitude');
             $table->boolean('is_approved')->default(true);
             $table->string('other_remark')->default('null');
-            $table->string('reason')->default('null');
             $table->string('sync_reason')->default('null');
             $table->string('aadhar_image')->default('null');
             $table->string('mgnrega_image')->default('null');
