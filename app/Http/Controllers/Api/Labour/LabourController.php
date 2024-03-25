009a881f2e83d71d35292335d83a7b0a913d18b5
Savita444 <savita.hajare@sumagoinfotech.com>
2024-03-25 10:13:59 +0530

    changes in api

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -815,20 +815,19 @@ public function updateLabourSecondForm(Request $request)
 
         $labour_data->save();
 
-        $familyDetails = [];
-        $familyDetailNew = json_decode($request->family, true);
-
-        foreach ($familyDetailNew as $familyMember) {
+        // $familyDetails = [];
+        $familyDetailNew = json_decode($request->family,true);
+            
+        foreach ($familyDetailNew as $key => $familyMember) {
             $familyDetail = new LabourFamilyDetails();
             $familyDetail->labour_id = $labour_data->id;
-            $familyDetail->full_name = $familyMember['full_name'];
-            $familyDetail->gender_id = $familyMember['gender_id'];
-            $familyDetail->relationship_id = $familyMember['relationship_id'];
-            $familyDetail->married_status_id = $familyMember['married_status_id'];
-            $familyDetail->date_of_birth = $familyMember['date_of_birth'];
-        
+            $familyDetail->full_name = $familyMember['fullName'];
+            $familyDetail->gender_id = $familyMember['genderId'];
+            $familyDetail->relationship_id = $familyMember['relationId'];
+            $familyDetail->married_status_id = $familyMember['maritalStatusId'];
+            $familyDetail->date_of_birth = $familyMember['dob'];
             $familyDetail->save();
-            $familyDetails[] = $familyDetail; 
+            $familyDetails[] = $familyDetail; // Collect family details
         }
 
         return response()->json(['status' => 'true', 'message' => 'Labour updated successfully', 'data' => $labour_data], 200);
