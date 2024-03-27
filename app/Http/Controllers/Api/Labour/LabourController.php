6f50d358f4607771e06c7178f12dcc07c1c2eca9
Savita444 <savita.hajare@sumagoinfotech.com>
2024-03-27 16:00:46 +0530

    changes innapi

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -86,9 +86,9 @@ class LabourController extends Controller
             
 
         ];
-        // if(isset($request->landline_number)) {
-        //     $all_data_validation['landline_number'] =  ['required', 'regex:/^[0-9]{8,}$/'];
-        // }
+        if(isset($request->landline_number)) {
+            $all_data_validation['landline_number'] =  ['required', 'regex:/^[0-9]{8,}$/'];
+        }
         $validator = Validator::make($request->all(), $all_data_validation);
 
 
@@ -112,12 +112,12 @@ class LabourController extends Controller
             $labour_data->taluka_id = $request->taluka_id;
             $labour_data->village_id = $request->village_id;
             $labour_data->mobile_number = $request->mobile_number;
-            $labour_data->landline_number = $request->landline_number;
+            // $labour_data->landline_number = $request->landline_number;
             $labour_data->mgnrega_card_id = $request->mgnrega_card_id;
             $labour_data->skill_id = $request->skill_id;
             $labour_data->latitude = $request->latitude;
             $labour_data->longitude = $request->longitude;
-            // $labour_data->landline_number = $request->has('landline_number') ? $request->landline_number : null;
+            $labour_data->landline_number = $request->has('landline_number') ? $request->landline_number : null;
             $labour_data->save();
 
             $last_insert_id = $labour_data->id;
