1965bd13c6ee75e22d421859cd21d3437c41e200
Satish <satishaurange@sumagoinfotech.com>
2024-03-31 13:00:42 +0530

    changes for the officer labor approvd  updated to 0 is_resubmitted

--- app/Http/Controllers/Api/Labour/OfficerController.php
+++ app/Http/Controllers/Api/Labour/OfficerController.php
@@ -235,6 +235,7 @@ class OfficerController extends Controller
             
            
             $updated = Labour::where('mgnrega_card_id', $request->mgnrega_card_id)
+                ->where('is_resubmitted', 0)
                 ->where('is_approved', 1)
                 ->update(['is_approved' => 2]); 
                 
