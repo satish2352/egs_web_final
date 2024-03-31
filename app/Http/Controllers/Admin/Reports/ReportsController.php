4d9cb9fad8a618478fc8604550d80211f73da055
mhaskemahesh <maheshmhaske500@gmail.com>
2024-03-31 17:22:08 +0530

    taluka not showing issue resolved

--- app/Http/Controllers/Admin/Reports/ReportsController.php
+++ app/Http/Controllers/Admin/Reports/ReportsController.php
@@ -36,13 +36,11 @@ class ReportsController extends Controller
             $sess_user_role=session()->get('role_id');
             $sess_user_working_dist=session()->get('working_dist');
 
-            $district_data = TblArea::where('location_type', 2) // 4 represents cities
-                        ->where('parent_id', '2')
+            $district_data = TblArea::where('parent_id', '2')
                         ->orderBy('name', 'asc')
                         ->get(['location_id', 'name']);
 
-            $taluka_data=TblArea::where('location_type', 3) // 4 represents cities
-                        ->where('parent_id', $sess_user_working_dist)
+            $taluka_data=TblArea::where('parent_id', $sess_user_working_dist)
                         ->orderBy('name', 'asc')
                         ->get(['location_id', 'name']);
 
