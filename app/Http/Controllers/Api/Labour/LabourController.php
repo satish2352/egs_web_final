e811a76ef2f184bc03da9d07fd43b5eed4a05db7
Savita444 <savita.hajare@sumagoinfotech.com>
2024-04-01 12:55:23 +0530

    changes in in count api

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -116,7 +116,7 @@ class LabourController extends Controller
             $labour_data->skill_id = $request->skill_id;
             $labour_data->latitude = $request->latitude;
             $labour_data->longitude = $request->longitude;
-            $labour_data->landline_number = $request->has('landline_number') ? $request->landline_number : "null";
+            $labour_data->landline_number = $request->has('landline_number') ? $request->landline_number : 'null';
             $labour_data->save();
 
             $last_insert_id = $labour_data->id;
