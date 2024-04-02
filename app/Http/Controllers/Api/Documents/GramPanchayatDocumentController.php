8fa5040e5dac5f8df53a93caad6cb95acca6c80e
Savita444 <savita.hajare@sumagoinfotech.com>
2024-04-02 14:52:55 +0530

    change in gramsevak doc not approved officer

--- app/Http/Controllers/Api/Documents/GramPanchayatDocumentController.php
+++ app/Http/Controllers/Api/Documents/GramPanchayatDocumentController.php
@@ -193,7 +193,7 @@ class GramPanchayatDocumentController extends Controller
             $validator = Validator::make($request->all(), [
                 'id' => 'required', 
                 // 'document_type_id' => 'required',
-                'document_name' => 'required',             
+                // 'document_name' => 'required',             
                 'document_pdf' => 'required|mimes:pdf|min:1|max:10240', 
             ]);
     
