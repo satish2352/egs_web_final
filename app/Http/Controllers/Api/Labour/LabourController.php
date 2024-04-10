6861edf577ffe9aa719afced2175561d8b6634ec
Savita444 <savita.hajare@sumagoinfotech.com>
2024-04-10 13:02:38 +0530

    changes in km

--- app/Http/Controllers/Api/Labour/LabourController.php
+++ app/Http/Controllers/Api/Labour/LabourController.php
@@ -319,6 +319,7 @@ class LabourController extends Controller
                         'tbl_reason.reason_name as reason_name',
                         'tbl_history.other_remark',
                         'tbl_history.updated_at',
+                        HistoryModel::raw("CONVERT_TZ(tbl_history.updated_at, '+00:00', '+05:30') as updated_at"), 
                     )
                     ->where('tbl_history.labour_id', $labourhistory['id'])
                     ->get();
